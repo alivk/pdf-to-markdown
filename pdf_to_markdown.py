e3ff4bf8a29867b9976c856c26a813e0fa53734a
alivk <86346768+alivk@users.noreply.github.com>
2023-04-13 18:48:10 +0800

    Update pdf_to_markdown.py

--- pdf_to_markdown.py
+++ pdf_to_markdown.py
@@ -17,19 +17,19 @@ for i, page in enumerate(pages):
     # Extract slide title from page using pdftotext command
     title_command = f"pdftotext -f {i+1} -l {i+1} -layout '{pdf_file}' -"
     content = subprocess.check_output(title_command, shell=True).decode("utf-8")
-    title = f'page-{i+1}-' + re.sub(r'\W+', '-', content.lower().strip()[:20])
+    title = f'pg-{i+1}-' + re.sub(r'\W+', '-', content.lower().strip()[:30])
     
     # Save image to disk
     image_folder = os.path.join(output_dir, f"{title}.assets")
     if not os.path.exists(image_folder):
         os.makedirs(image_folder)
-    image_file = os.path.join(image_folder, f"page-{i+1}.png")
+    image_file = os.path.join(image_folder, f"pg-{i+1}.png")
     page.save(image_file, "PNG")
 
     # Generate markdown file content
     markdown_content = f"""# {title}
 ### PPT Slide
-<img src="{title}.assets/page-{i+1}.png" alt="" style="float:left;width:600px;" />
+<img src="{title}.assets/pg-{i+1}.png" alt="" style="float:left;width:600px;" />
 
 ### Speaker Notes
 This is the speaker notes for slide {i+1}.
